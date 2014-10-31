4a3d22a22a8d98678e09d1f97cd5fc6139c61468
NuclearCookie <pietervantorre@gmail.com>
2014-10-31 15:28:23 +0100

    Implemented enemy movement.

---
+++ Assets/Code/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+	private EnemyProperties Properties;
+	private GameObject Tower;
+	// Use this for initialization
+	void Start()
+	{
+		Properties = gameObject.GetComponent<EnemyProperties>();
+		DebugUtils.Assert(Properties != null);
+		Tower = GameObject.FindWithTag("Tower");
+		DebugUtils.Assert(Tower != null);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 direction = Tower.transform.position - transform.position;
+		transform.position += Properties.Speed * direction * Time.deltaTime;
+	}
+}
---
+++ Assets/Code/EnemyProperties.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyProperties : MonoBehaviour {
+
+    public float HealthPoints;
+    public bool Shield;
+    public float DamageAmount;
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
