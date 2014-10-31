43abc8f0cdd88a9ac5eb2ec9cf9294be3abad35b
NuclearCookie <pietervantorre@gmail.com>
2014-10-31 19:05:00 +0100

    Improved performance for multiple agents. Added targets around the tower and always take the closest target.

--- Assets/Code/EnemyProperties.cs
+++ Assets/Code/EnemyProperties.cs
@@ -1,19 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Pathfinding;
 
+[RequireComponent(typeof(AIPath))]
 public class EnemyProperties : MonoBehaviour {
 
 	public float HealthPoints;
 	public bool Shield;
 	public float DamageAmount;
+	private AIPath m_AIPath;
+	private GameObject m_Tower;
 
 	// Use this for initialization
 	void Start () {
-	
+		m_Tower = GameObject.FindGameObjectWithTag("Tower");
+		DebugUtils.Assert(m_Tower != null, "No object with tag \"Tower\" was found");
+		m_AIPath = gameObject.GetComponent<AIPath>();
+		m_AIPath.target = GetClosestTarget();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	Transform GetClosestTarget()
+	{
+		Transform returnValue = m_Tower.transform;
+		Vector3 shortestDistance = m_Tower.transform.position - gameObject.transform.position;
+		foreach(Transform child in m_Tower.transform)
+		{
+			Debug.Log("Checking children");
+			Vector3 tempDistance;
+			tempDistance = child.position - gameObject.transform.position;
+			if (tempDistance.sqrMagnitude < shortestDistance.sqrMagnitude)
+			{
+				returnValue = child.transform;
+				shortestDistance = tempDistance;
+			}
+		}
+		return returnValue;
 	}
 }
