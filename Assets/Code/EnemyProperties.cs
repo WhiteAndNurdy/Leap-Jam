b6fef42d748ee4b7d2756c0df87ef7064ed151c8
NuclearCookie <pietervantorre@gmail.com>
2014-10-31 17:55:51 +0100

    Changed enemy move script to use AIPath script. Path is now recalculated often to have correct use of dynamic obstacles

--- Assets/Code/EnemyProperties.cs
+++ Assets/Code/EnemyProperties.cs
@@ -6,7 +6,6 @@ public class EnemyProperties : MonoBehaviour {
 	public float HealthPoints;
 	public bool Shield;
 	public float DamageAmount;
-	public float Speed;
 
 	// Use this for initialization
 	void Start () {
