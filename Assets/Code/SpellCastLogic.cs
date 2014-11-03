7a7553a5e2a399a1c84132a93da821fdae0bfe50
NuclearCookie <pietervantorre@gmail.com>
2014-11-03 19:45:29 +0100

    Added aim indicator for the future hand controller. An object is rendered on the place of the (currently mouse) position when you press 'A'. The object is always rendered on top, through a second camera.

---
+++ Assets/Code/SpellCastLogic.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellCastLogic : MonoBehaviour {
+
+	public LayerMask RayCastLayerMask;
+	public float RayCastRange = 100.0f;
+
+	private GameObject m_AimIndicator;
+	// Use this for initialization
+	void Start () 
+	{
+		m_AimIndicator = transform.Find("AimIndicator").gameObject;
+		DebugUtils.Assert(m_AimIndicator != null, "Couldn't find child AimIndicator");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (IsAiming())
+		{
+			Aim();
+		}
+		else
+		{
+			m_AimIndicator.SetActive(false);
+		}
+	}
+
+	void Aim()
+	{
+		Ray ray;
+		RaycastHit hit;
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit, RayCastRange, RayCastLayerMask))
+		{
+			if (hit.collider.name == "FloorPlane")
+			{
+
+				m_AimIndicator.transform.position = hit.point;
+				m_AimIndicator.SetActive(true);
+			}
+		}
+	}
+
+	bool IsAiming()
+	{
+		return Input.GetKey(KeyCode.A);
+	}
+}
