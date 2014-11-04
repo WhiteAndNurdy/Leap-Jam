a82f08e40ecd2249996403ab89b1b13cfdb88a77
NuclearCookie <pietervantorre@gmail.com>
2014-11-04 23:15:01 +0100

    Spells are removed after a short time

---
+++ Assets/Code/SpellLogic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellLogic : MonoBehaviour {
+
+	public float TEMP__LifeTime = 1.0f;
+	// Use this for initialization
+	void Start () {
+		GameObject.Destroy(gameObject, TEMP__LifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
