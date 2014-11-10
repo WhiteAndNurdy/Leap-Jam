935f2c5f8cf717ea9f79f460ee69edd16b75aa42
NuclearCookie <pietervantorre@gmail.com>
2014-11-10 17:05:19 +0100

    Readded changes to SpellCastLogic file and fixed nullreferences in enemylogic

--- Assets/Code/EnemyLogic.cs
+++ Assets/Code/EnemyLogic.cs
@@ -8,14 +8,17 @@ public class EnemyLogic : EntityLogic
 	private GameObject m_Tower;
 	private GameObject m_Shield;
 
+	void Awake()
+	{
+		m_EnemyProperties = GetComponent<EnemyProperties>();
+		m_Tower = GameObject.FindGameObjectWithTag("Tower");
+		m_Shield = transform.FindChild("Shield").gameObject;
+	}
 	// Use this for initialization
 	protected override void Start()
 	{
 		base.Start();
-		m_EnemyProperties = GetComponent<EnemyProperties>();
-		m_Tower = GameObject.FindGameObjectWithTag("Tower");
 		DebugUtils.Assert(m_Tower != null, "Tower object not found!");
-		m_Shield = transform.FindChild("Shield").gameObject;
 		DebugUtils.Assert(m_Shield != null, "Shield child not found!");
 	}
 
@@ -59,6 +62,9 @@ public class EnemyLogic : EntityLogic
 
 	void DoAttack()
 	{
+		DebugUtils.Assert(m_Tower != null, "Tower NULL!");
+		DebugUtils.Assert(m_Tower.GetComponent<TowerLogic>() != null, "TowerLogic NULL!");
+		DebugUtils.Assert(m_EnemyProperties != null, "EnemyProperties NULL!");
 		m_Tower.GetComponent<TowerLogic>().Damage(m_EnemyProperties.DamageAmount);
 	}
 }
