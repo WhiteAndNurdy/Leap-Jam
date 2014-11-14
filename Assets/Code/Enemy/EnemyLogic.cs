6baadb99a81d44b86c78c511af5cde0d183b869b
NuclearCookie <pietervantorre@gmail.com>
2014-11-14 21:39:52 +0100

    Moved Path Update function from properties file to logic file

--- Assets/Code/Enemy/EnemyLogic.cs
+++ Assets/Code/Enemy/EnemyLogic.cs
@@ -2,17 +2,22 @@
 using System.Collections;
 
 [RequireComponent(typeof(EnemyProperties))]
+[RequireComponent(typeof(AIPath))]
 public class EnemyLogic : EntityLogic
 {
+	public float ReassignTargetRate = 0.2f;
+
 	private EnemyProperties m_EnemyProperties;
 	private GameObject m_Tower;
 	private GameObject m_Shield;
+	private AIPath m_AIPath;
 
 	void Awake()
 	{
 		m_EnemyProperties = GetComponent<EnemyProperties>();
 		m_Tower = GameObject.FindGameObjectWithTag("Tower");
 		m_Shield = transform.FindChild("Shield").gameObject;
+		m_AIPath = gameObject.GetComponent<AIPath>();
 	}
 	// Use this for initialization
 	protected override void Start()
@@ -20,12 +25,36 @@ public class EnemyLogic : EntityLogic
 		base.Start();
 		DebugUtils.Assert(m_Tower != null, "Tower object not found!");
 		DebugUtils.Assert(m_Shield != null, "Shield child not found!");
+		StartCoroutine("UpdateAIPath");
 	}
 
-	// Update is called once per frame
-	protected override void Update()
+	IEnumerator UpdateAIPath()
 	{
-		base.Update();
+		while (true)
+		{
+			if (m_EnemyProperties.EnemyActive)
+			{
+				m_AIPath.target = GetClosestTarget();
+			}
+			yield return new WaitForSeconds(ReassignTargetRate);
+		}
+	}
+
+	Transform GetClosestTarget()
+	{
+		Transform returnValue = m_Tower.transform;
+		Vector3 shortestDistance = m_Tower.transform.position - gameObject.transform.position;
+		foreach (GameObject target in GameObject.FindGameObjectsWithTag("Agent_Targets"))
+		{
+			Vector3 tempDistance;
+			tempDistance = target.transform.position - gameObject.transform.position;
+			if (tempDistance.sqrMagnitude < shortestDistance.sqrMagnitude)
+			{
+				returnValue = target.transform.transform;
+				shortestDistance = tempDistance;
+			}
+		}
+		return returnValue;
 	}
 
 	IEnumerator CheckForAttack()
@@ -39,6 +68,7 @@ public class EnemyLogic : EntityLogic
 
 	public void Attack()
 	{
+		StopCoroutine("UpdateAIPath");
 		StartCoroutine("CheckForAttack");
 	}
 
--- Assets/Code/Enemy/EnemyProperties.cs
+++ Assets/Code/Enemy/EnemyProperties.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Pathfinding;
 
-[RequireComponent(typeof(AIPath))]
 public class EnemyProperties : EntityProperties {
 
 	public bool Shield;
@@ -18,11 +17,9 @@ public class EnemyProperties : EntityProperties {
 		}
 	}
 	public float DamageAmount;
-	public float ReassignTargetRate = 0.2f;
 	public float TimeBetweenAttacks;
 	public Elements[] VulnerableTo = new Elements[1];
 
-	private AIPath m_AIPath;
 	private GameObject m_Tower;
 	private HashSet<Elements> VulnerableToUnique = new HashSet<Elements>();
 	private bool m_EnemyActive;
@@ -30,7 +27,6 @@ public class EnemyProperties : EntityProperties {
 	void Awake()
 	{
 		m_Tower = GameObject.FindGameObjectWithTag("Tower");
-		m_AIPath = gameObject.GetComponent<AIPath>();
 	}
 
 	// Use this for initialization
@@ -38,43 +34,12 @@ public class EnemyProperties : EntityProperties {
 	{
 		base.Start();
 		DebugUtils.Assert(m_Tower != null, "No object with tag \"Tower\" was found");
-		m_AIPath.target = GetClosestTarget();
 		foreach (Elements element in VulnerableTo)
 		{
 			VulnerableToUnique.Add(element);
 		}
 		// dereference array that should not be used.
 		VulnerableTo = null;
-		StartCoroutine("UpdateAIPath");
-	}
-	
-	IEnumerator UpdateAIPath()
-	{
-		while (true)
-		{
-			if(EnemyActive)
-			{
-				m_AIPath.target = GetClosestTarget();
-			}
-			yield return new WaitForSeconds(ReassignTargetRate);
-		}
-	}
-
-	Transform GetClosestTarget()
-	{
-		Transform returnValue = m_Tower.transform;
-		Vector3 shortestDistance = m_Tower.transform.position - gameObject.transform.position;
-		foreach(GameObject target in GameObject.FindGameObjectsWithTag("Agent_Targets"))
-		{
-			Vector3 tempDistance;
-			tempDistance = target.transform.position - gameObject.transform.position;
-			if (tempDistance.sqrMagnitude < shortestDistance.sqrMagnitude)
-			{
-				returnValue = target.transform.transform;
-				shortestDistance = tempDistance;
-			}
-		}
-		return returnValue;
 	}
 
 	public bool IsVulnerableTo(Elements type)
